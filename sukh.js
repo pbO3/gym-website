c7d5ef88d3b8abbe03f90b18dded289580df9135
sukhjit singh <you@example.com>
2023-05-16 20:59:03 +0530

    new website made

---
+++ sukh.js
@@ -0,0 +1,18 @@
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0});
+
+videos.forEach((video) => {
+    ScrollTrigger.create({
+        trigger: video,
+        start: "top center",
+        end: "bottom center",
+        marker: true,
+        onEnter: () => {
+            gsap.to(video, {opacity: 1});
+            video.play();
+        },
+        onEnterBack: () => video.play(),
+        onLeave: () => video.pause(),
+        onLeaveBack: () => video.pause(),
+    });
+});
